9f7a6bf51a11b68369374483ea46321b37c82dcb
Hoakin26 <joaquingsuy93@gmail.com>
2024-05-16 11:10:57 -0600

    Creacion de login

---
+++ proyecto_LOGIN_EN_C/Form1.h
@@ -0,0 +1,219 @@
+#pragma once
+
+// uncomment to execute the rk1-utils:
+//    #include "rk1_Utils_demo.h"  // shows how the rk1-utils can be used
+
+#include "Header1.h"
+#include "Header2.h"
+#include "MySQLConnector.h"
+
+namespace CppCLRWinFormsProject {
+
+  using namespace System;
+  using namespace System::ComponentModel;
+  using namespace System::Collections;
+  using namespace System::Windows::Forms;
+  using namespace System::Data;
+  using namespace System::Drawing;
+
+  /// <summary>
+  /// Summary for Form1
+  /// </summary>
+  public ref class Form1 : public System::Windows::Forms::Form
+  {
+  public:
+      MySQLConnector^ connector = gcnew MySQLConnector();
+      MySqlConnection^ connection;
+    Form1(void)
+    {
+      InitializeComponent();
+    
+
+      connection = gcnew MySqlConnection(connector->connectionString);
+      //
+      //TODO: Add the constructor code here
+      //
+
+      // uncomment to execute the rk1-utils:
+      //    N_rk1_Utils_demo::execute(); // shows how the rk1-utils can be used
+
+    }
+
+  protected:
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    ~Form1()
+    {
+      if (components)
+      {
+        delete components;
+      }
+    }
+  private: System::Windows::Forms::TextBox^ txtUsuario;
+  private: System::Windows::Forms::TextBox^ txtContraseña;
+  protected:
+
+  protected:
+
+  private: System::Windows::Forms::Label^ label1;
+  private: System::Windows::Forms::Label^ label2;
+  private: System::Windows::Forms::Button^ txtEntrarAlSistema;
+
+
+
+
+
+  protected:
+
+  private:
+      System::ComponentModel::Container^ components;
+     
+
+
+
+
+
+#pragma region Windows Form Designer generated code
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    void InitializeComponent(void)
+    {
+        this->txtUsuario = (gcnew System::Windows::Forms::TextBox());
+        this->txtContraseña = (gcnew System::Windows::Forms::TextBox());
+        this->label1 = (gcnew System::Windows::Forms::Label());
+        this->label2 = (gcnew System::Windows::Forms::Label());
+        this->txtEntrarAlSistema = (gcnew System::Windows::Forms::Button());
+        this->SuspendLayout();
+        // 
+        // txtUsuario
+        // 
+        this->txtUsuario->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+        this->txtUsuario->Location = System::Drawing::Point(84, 102);
+        this->txtUsuario->Name = L"txtUsuario";
+        this->txtUsuario->Size = System::Drawing::Size(275, 35);
+        this->txtUsuario->TabIndex = 0;
+        // 
+        // txtContraseña
+        // 
+        this->txtContraseña->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+        this->txtContraseña->Location = System::Drawing::Point(81, 190);
+        this->txtContraseña->Name = L"txtContraseña";
+        this->txtContraseña->Size = System::Drawing::Size(275, 35);
+        this->txtContraseña->TabIndex = 1;
+        // 
+        // label1
+        // 
+        this->label1->AutoSize = true;
+        this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+        this->label1->Location = System::Drawing::Point(76, 65);
+        this->label1->Name = L"label1";
+        this->label1->Size = System::Drawing::Size(133, 29);
+        this->label1->TabIndex = 2;
+        this->label1->Text = L"USUARIO:";
+        // 
+        // label2
+        // 
+        this->label2->AutoSize = true;
+        this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+        this->label2->Location = System::Drawing::Point(79, 158);
+        this->label2->Name = L"label2";
+        this->label2->Size = System::Drawing::Size(194, 29);
+        this->label2->TabIndex = 3;
+        this->label2->Text = L"CONTRASEÑA:";
+        // 
+        // txtEntrarAlSistema
+        // 
+        this->txtEntrarAlSistema->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+        this->txtEntrarAlSistema->Location = System::Drawing::Point(96, 251);
+        this->txtEntrarAlSistema->Name = L"txtEntrarAlSistema";
+        this->txtEntrarAlSistema->Size = System::Drawing::Size(244, 45);
+        this->txtEntrarAlSistema->TabIndex = 4;
+        this->txtEntrarAlSistema->Text = L"Entrar al sistema";
+        this->txtEntrarAlSistema->UseVisualStyleBackColor = true;
+        this->txtEntrarAlSistema->Click += gcnew System::EventHandler(this, &Form1::txtEntrarAlSistema_Click);
+        // 
+        // Form1
+        // 
+        this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+        this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+        this->ClientSize = System::Drawing::Size(442, 308);
+        this->Controls->Add(this->txtEntrarAlSistema);
+        this->Controls->Add(this->label2);
+        this->Controls->Add(this->label1);
+        this->Controls->Add(this->txtContraseña);
+        this->Controls->Add(this->txtUsuario);
+        this->Name = L"Form1";
+        this->Text = L"Form1";
+        this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+        this->ResumeLayout(false);
+        this->PerformLayout();
+
+    }
+#pragma endregion
+
+   // You can call the functions at a button click. If you prefer, 
+    // you can call them by clicking a menu item.
+
+
+
+  private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+      
+      
+      
+  }
+private: System::Void txtEntrarAlSistema_Click(System::Object^ sender, System::EventArgs^ e) {
+    try 
+{
+
+       
+            // Obtener el usuario y la contraseña ingresados por el usuario
+            String^ usuario = txtUsuario->Text;
+            String^ contraseña = txtContraseña->Text;
+
+            // Consulta SQL para verificar si el usuario y la contraseña son válidos
+            String ^ query = "SELECT COUNT(*) FROM login WHERE usuario = '" + usuario + "' AND contraseña = '" + contraseña + "'";
+
+            // Abrir la conexión antes de ejecutar la consulta
+            if (connector->OpenConnection()) {
+                MySqlCommand^ command = gcnew MySqlCommand(query, connector->getConnection());
+                int count = Convert::ToInt32(command->ExecuteScalar());
+
+                // Cerrar la conexión después de ejecutar la consulta
+                connector->CloseConnection();
+
+                // Verificar si se encontró una coincidencia en la base de datos
+                if (count > 0) {
+                    MessageBox::Show("Inicio de sesión exitoso. Bienvenido, " + usuario + "!");
+                    connector->CloseConnection();
+                }
+                else {
+                    MessageBox::Show("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.");
+                    connector->CloseConnection();
+                }
+            }
+            else {
+                MessageBox::Show("Conector no ejecutado.");
+            }
+      
+       
+        
+
+        
+    }
+    catch (Exception^ ex) {
+        MessageBox::Show(" error: " + ex->Message);
+        connector->CloseConnection();
+    }
+    finally {
+        if (connection->State == ConnectionState::Open) {
+            connector->CloseConnection();
+        }
+    }
+
+}
+}; // end of class Form1
+} // end of namespace CppCLRWinFormsProject
+
---
+++ proyecto_LOGIN_EN_C/MySQLConnector.h
@@ -0,0 +1,68 @@
+
+#include <iostream>
+
+
+using namespace System;
+using namespace MySql::Data::MySqlClient;
+using namespace std;
+using namespace System::Data;
+
+public ref class MySQLConnector {
+private:
+    MySqlConnection^ connection;
+
+public:
+    String^ connectionString;
+    String^ host = "localhost";
+    String^ usuario = "root";
+    String^ contraseña = "";
+    String^ database = "bd_registo_personas";
+
+    MySQLConnector() : connection(nullptr) 
+    {
+        connectionString = "datasource=127.0.0.1; username=root; password=""; database=bd_registro_persona";
+        //connectionString = "Server=" + host + ";Uid=" + usuario + ";Pwd=" + contraseña + ";Database=" + database + ";";
+        connection = gcnew MySqlConnection(connectionString);
+    }
+
+    bool OpenConnection() {
+        try {
+            connection->Open();
+            return true;
+        }
+        catch (Exception^ ex) {
+           
+            return false;
+        }
+    }
+
+    void CloseConnection() {
+        if (connection != nullptr && connection->State == ConnectionState::Open) {
+            connection->Close();
+           
+        }
+    }
+
+    MySqlConnection^ getConnection() {
+        return connection;
+    }
+
+    ~MySQLConnector() {
+        CloseConnection();
+    }
+};
+
+//int main() {
+//    
+//
+//    MySQLConnector^ connector = gcnew MySQLConnector();
+//
+//    if (connector->OpenConnection()) {
+//        // Realiza operaciones con la base de datos...
+//        // Por ejemplo, consultas, inserciones, actualizaciones, etc.
+//
+//        connector->CloseConnection(); // Cierra la conexión cuando hayas terminado de usarla
+//    }
+//
+//    return 0;
+//}
